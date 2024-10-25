86e7934a2e19617ebc473282f47e14df6e377e71
KamalJamal <husnetdinovk94@gmail.com>
2024-10-25 13:55:05 +0300

    removed all unnecessary files

--- dist/modifyBodyRequest/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.modifyBodyRequest = modifyBodyRequest;
-function modifyBodyRequest(oldBody, newDataforBody) {
-    oldBody.formdata[newDataforBody.phone_index].value = newDataforBody.phone;
-    oldBody.formdata[newDataforBody.country_index].value = newDataforBody.ip;
-    return oldBody;
-}
-//# sourceMappingURL=index.js.map
